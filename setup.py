438b5a093c9e65fb7f40b07300a6300f97c0056c
Rohit Kapur <rkapur@flatiron.com>
2019-11-14 15:32:43 -0500

    bumped version to 0.0.3

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ with open("README.md", "r") as fh:
 
 setup(
     name=NAME,
-    version="0.0.2",
+    version="0.0.3",
     entry_points={
         "console_scripts": ["fh-immuta-utils = fh_immuta_utils.scripts.cli.main_cli"]
     },
