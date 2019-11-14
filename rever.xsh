a68505f29087ca3abf623018d099b3cf7a13b8d1
Marius van Niekerk <marius.v.niekerk@gmail.com>
2019-11-14 11:10:22 -0500

    Add rever to handle releasing the package

---
+++ rever.xsh
@@ -0,0 +1,14 @@
+#!/usr/bin/env xonsh
+
+$PROJECT = 'fh-immuta-utils'
+$ACTIVITIES = [
+	'version_bump',
+	'tag',
+	'push_tag',
+	'pypi',
+	'ghrelease'
+]
+	
+$GITHUB_ORG = 'flatironhealth'
+$GITHUB_REPO = 'fh-immuta-utils'
+
--- setup.py
+++ setup.py
@@ -4,25 +4,26 @@ from setuptools import setup, find_packages  # noqa: H301
 
 NAME = "fh-immuta-utils"
 VERSION = "0.0.1"
-# To install the library, run the following
-#
-# python setup.py install
-#
-# prerequisite: setuptools
-# http://pypi.python.org/pypi/setuptools
 
-REQUIRES = ["urllib3 >= 1.15", "six >= 1.10", "certifi", "hvac", "python-dateutil"]
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
 
 setup(
     name=NAME,
     version=VERSION,
     description="Flatiron Immuta API",
     author_email="data-tooling@flatiron.com",
-    url="",
-    install_requires=REQUIRES,
+    url="https://github.com/flatironhealth/fh-immuta-utils",
     packages=find_packages(),
     include_package_data=True,
-    long_description="""\
-    Wrapper around Immuta's API to manage data sources and permissions in Immuta.
-    """,
+    long_description=long_description,
+    classifiers = [
+        "Programming Language :: Python :: 3 :: Only",
+        "License  :: OSI Approved :: BSD License",
+        "Operating System :: OS Independent",
+        "Development Status :: 5 - Production/Stable",
+    ],
+    python_requires='>=3.6',
 )
